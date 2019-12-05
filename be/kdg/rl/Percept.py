b7d9da6f2541e2e897c6f843e9e8a3590c2f0e44
Mees van Kaam <mees.vankaam@student.kdg.be>
2019-12-05 15:16:42 +0100

    added getters in percept

--- be/kdg/rl/Percept.py
+++ be/kdg/rl/Percept.py
@@ -4,11 +4,32 @@ import numpy
 class Percept:
     #TODO: Hier datatypes bijvoegen?
     def __init__(self, s, a, r, t, final):
-        self.s = s
-        self.a = a
-        self.r = r
-        self.t = t
-        self.final = final
+        self._s = s
+        self._a = a
+        self._r = r
+        self._t = t
+        self._final = final
+
+    @property
+    def s(self):
+        return self._s
+
+    @property
+    def a(self):
+        return self._a
+
+    @property
+    def r(self):
+        return self._r
+
+    @property
+    def t(self):
+        return self._t
+
+    @property
+    def final(self):
+        return self._final
+
 
     def __repr__(self):
         return "state: " + str(self.s) + "\naction: " + str(self.a) + "\nreward: " + str(self.r) + "\nnext state: " \
